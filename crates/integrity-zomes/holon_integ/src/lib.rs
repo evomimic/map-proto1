985bf027aa5417058e096aadd3999fe09f8af01d
evomimic <evomimic@gmail.com>
2022-12-28 15:08:18 -0700

    framing the code structure architecture

---
+++ crates/integrity-zomes/holon_integ/src/lib.rs
@@ -0,0 +1,17 @@
+#[path = "holon.rs"]
+pub mod holon;
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
